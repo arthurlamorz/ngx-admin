5b418ff732382d66458ab2096ae4a0edcaac379c
Arthur S. Lamorz <arthur.lamorz@gmail.com>
2018-02-14 20:55:34 +0800

    table styles

--- src/app/pages/tables/language-table/language-table.component.ts
+++ src/app/pages/tables/language-table/language-table.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { LocalDataSource } from 'ng2-smart-table';
+import { Observable } from 'rxjs/Observable';
+import { ToasterService, Toast, ToasterConfig, BodyOutputType } from 'angular2-toaster';
 
-import { LanguageService } from '../../../services/cms-services/language.service';
+import { LanguageService, LanguageDetails } from '../../../services/cms-services/language.service';
 
 @Component({
   selector: 'ngx-language-table',
   templateUrl: './language-table.component.html',
+  styleUrls: ['./language-table.component.scss'],
   styles: [`
     nb-card {
       transform: translate3d(0, 0, 0);
@@ -46,25 +49,69 @@ export class LanguageTableComponent implements OnInit {
   };
 
   source: LocalDataSource = new LocalDataSource();
-
+  toasterConfig: ToasterConfig;
   constructor(
     private languageService: LanguageService,
+    private toasterService: ToasterService,
   ) {
 
   }
 
   ngOnInit(): void {
     const self = this;
-    self.languageService.getLanguage('Silvertooth', 'zh-HANT')
-      .subscribe(r => {
-        self.source.load(r.mappings)
-      }, error => {
-        alert(JSON.stringify(error));
-      });
 
     self.languageService.getlanguageList('Silvertooth')
       .subscribe(r => {
-        alert(JSON.stringify(r));
+        const languageMappingsObservables: Observable<LanguageDetails>[] = [];
+
+        r.languages.forEach(
+          l => languageMappingsObservables.push(
+            self.languageService.getLanguage('Silvertooth', l)));
+
+        Observable.forkJoin(languageMappingsObservables).subscribe(results => {
+          results.forEach(l => self.source.load(l.mappings));
+        }, error => {
+          self.toasterConfig = new ToasterConfig({
+            positionClass: 'toast-top-full-width',
+            timeout: 0,
+            newestOnTop: true,
+            tapToDismiss: true,
+            preventDuplicates: true,
+            animation: 'fade',
+            limit: 5,
+          });
+
+          const toast: Toast = {
+            type: 'error',
+            title: 'Oops! Error',
+            body: 'App cards failed: ' + error.message,
+            timeout: 0,
+            showCloseButton: true,
+            bodyOutputType: BodyOutputType.TrustedHtml,
+          };
+          this.toasterService.popAsync(toast);
+        }, () => {
+          self.toasterConfig = new ToasterConfig({
+            positionClass: 'toast-top-full-width',
+            timeout: 0,
+            newestOnTop: true,
+            tapToDismiss: true,
+            preventDuplicates: true,
+            animation: 'fade',
+            limit: 5,
+          });
+
+          const toast: Toast = {
+            type: 'info',
+            title: 'OK',
+            body: 'Completed',
+            timeout: 5,
+            showCloseButton: true,
+            bodyOutputType: BodyOutputType.TrustedHtml,
+          };
+          this.toasterService.popAsync(toast);
+        });
+
       }, error => {
         alert(JSON.stringify(error));
       });
