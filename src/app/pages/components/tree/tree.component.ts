294725129a4098bc733c069ab88324bf2857e512
Arthur S. Lamorz <arthur.lamorz@gmail.com>
2018-03-31 15:45:43 +0800

    add menu function to tree

--- src/app/pages/components/tree/tree.component.ts
+++ src/app/pages/components/tree/tree.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { TreeModel } from 'ng2-tree';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
+import { TreeModel, NodeMenuItemAction } from 'ng2-tree';
 import { ActivatedRoute } from '@angular/router';
 
 import { ToasterService, Toast, ToasterConfig, BodyOutputType } from 'angular2-toaster';
@@ -15,41 +15,22 @@ import 'style-loader!angular2-toaster/toaster.css';
 })
 export class TreeComponent implements OnInit, OnDestroy {
 
+  @ViewChild('treeComponent') treeComponent;
+
   private sub;
   public appId;
 
-  tree: TreeModel = {
-    settings: {
-      'rightMenu': true,
-      menuItems: [
-        { action: null, name: 'Add parent node', cssClass: '' },
-        { action: null, name: 'Add child node', cssClass: '' },
-        { action: null, name: 'Remove node', cssClass: '' },
-        { action: null, name: 'Rename node', cssClass: '' },
-        { action: null, name: 'Custom Action', cssClass: '' },
-      ],
-    },
-    value: 'Programming languages by programming paradigm',
-    children: [{
-      value: 'Object-oriented programming',
-      children: [{
-        value: 'Java',
-      }, {
-        value: 'C++',
-      }, {
-        value: 'C#',
-      }],
-    }, {
-      value: 'Prototype-based programming',
-      children: [{
-        value: 'JavaScript',
-      }, {
-        value: 'CoffeeScript',
-      }, {
-        value: 'Lua',
-      }],
-    }],
-  };
+  private menuItemsMap = [
+    { 'key': 'addString', 'value': 'Add string field', 'type': 'string' },
+    { 'key': 'addNumber', 'value': 'Add number field', 'type': 'number' },
+    { 'key': 'addBoolean', 'value': 'Add boolean field', 'type': 'boolean' },
+    { 'key': 'addObject', 'value': 'Add an object', 'type': 'object' },
+    { 'key': 'rename', 'value': 'Rename', 'type': null },
+    { 'key': 'delete', 'value': 'Delete', 'type': null },
+  ]
+
+  private tree: TreeModel;
+
 
   public toasterConfig: ToasterConfig;
   constructor(
@@ -60,9 +41,49 @@ export class TreeComponent implements OnInit, OnDestroy {
 
   }
 
-
   ngOnInit(): void {
     const self = this;
+
+    const menuItems = this.menuItemsMap
+      .map(i => {
+        return {
+          'action': NodeMenuItemAction.Custom,
+          'name': i.value,
+          'cssClass': '',
+        }
+      })
+
+    self.tree = {
+      settings: {
+        'rightMenu': true,
+        menuItems: menuItems,
+      },
+      value: 'Programming languages by programming paradigm',
+      type: 'object',
+      id: 0,
+      children: [{
+        value: 'Object-oriented programming',
+        id: 1,
+        children: [{
+          value: 'Java', type: 'string', id: 11,
+        }, {
+          value: 'C++', type: 'string', id: 12,
+        }, {
+          value: 'C#', type: 'string', id: 13,
+        }],
+      }, {
+        value: 'Prototype-based programming',
+        type: 'object',
+        id: 2,
+        children: [{
+          value: 'JavaScript', type: 'string', id: 21,
+        }, {
+          value: 'CoffeeScript', type: 'string', id: 22,
+        }, {
+          value: 'Lua', type: 'string', id: 23,
+        }],
+      }],
+    };
 
     self.sub = self.route.params.subscribe(params => {
       self.appId = params['appid'];
@@ -95,6 +116,46 @@ export class TreeComponent implements OnInit, OnDestroy {
         };
         this.toasterService.popAsync(toast);
       });
+  }
+
+  onMenuItemSelected($event): void {
+    const self = this;
+
+    const node = $event.node;
+    const actionItem = self.menuItemsMap.filter(
+      i => i.value === $event.selectedItem,
+    )[0];
+    const nodeController = self.treeComponent.getControllerByNodeId(node.id);
+    if (actionItem.key === 'delete') {
+      nodeController.remove();
+    } else if (actionItem.key === 'rename') {
+      nodeController.startRenaming();
+    } else {
+
+      const menuItems = this.menuItemsMap
+        .map(i => {
+          return {
+            'action': NodeMenuItemAction.Custom,
+            'name': i.value,
+            'cssClass': '',
+          }
+        })
+
+      const childNode: TreeModel = {
+        'value': 'name here',
+        'type': actionItem.type,
+        'id' : Math.floor(99999999999 * Math.random()),
+        settings: {
+          'rightMenu': true,
+          menuItems: menuItems,
+        },
+      };
+
+      if (actionItem.type === 'object')
+        childNode.children = []
+
+      nodeController.addChild(childNode);
+    }
   }
 
   ngOnDestroy(): void {
